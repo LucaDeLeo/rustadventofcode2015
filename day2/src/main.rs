c382109fcdfb9c23885a7dde1e2b824bd09e9bf9
Luca <ldeleo@dc.uba.ar>
2021-10-22 18:11:09 -0300

    finished day 7

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,17 +4,18 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Couldn't read input");
 
-    let mut fribbon: i32 = 0;
-    let mut sum: i32 = 0;
-    let mut nums: Vec<i32> = vec![0, 0, 0];
+    let mut fribbon: u32 = 0;
+    let mut sum: u32 = 0;
+    let mut nums: Vec<u32> = vec![0, 0, 0];
     let mut i: usize;
     for l in input.lines() {
         i = 0;
+
         for n in l.split('x') {
-            nums[i] = n.parse::<i32>().unwrap();
+            nums[i] = n.parse::<u32>().unwrap();
             i += 1;
         }
-        let (a, b, c): (i32, i32, i32) = (nums[0], nums[1], nums[2]);
+        let (a, b, c): (u32, u32, u32) = (nums[0], nums[1], nums[2]);
         sum += 2 * (a * b + a * c + b * c) + min(min(a * b, a * c), b * c);
         fribbon += 2 * (a + b + c - nums.iter().max().unwrap()) + a * b * c;
     }
--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 #[derive(Debug)]
+#[derive(PartialEq)]
 enum Instruction {
     AND,
     OR,
@@ -9,82 +10,121 @@ enum Instruction {
     ASSIGN,
 }
 #[derive(Debug)]
-struct Gate<'a> {
-    pam1: &'a str,
+struct Gate {
+    pam1: String,
     ins: Instruction,
-    pam2: &'a str,
-    dest: &'a str,
+    pam2: String,
+    dest: String,
 }
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Couldn't read input");
-    let mut ins: Vec<Gate> = Vec::new();
+    let mut ins: Vec<Gate> = Vec::with_capacity(input.lines().count());
+    let mut part2 = false;
+    let mut a = "".to_string();
 
-    for line in input.lines() {
-        let mut tmp: Vec<&str> = line.split_whitespace().collect::<Vec<&str>>();
-        tmp.reverse();
-        let g = Gate {
-            pam1: if tmp.len() == 5 { tmp[4] } else { &" " },
-            ins: if tmp.len() == 4 {
-                Instruction::NOT
-            } else if tmp.len() == 5 {
-                match tmp[3] {
-                    "AND" => Instruction::AND,
-                    "OR" => Instruction::OR,
-                    "NOT" => Instruction::NOT,
-                    "RSHIFT" => Instruction::RSHIFT,
-                    "LSHIFT" => Instruction::LSHIFT,
-                    _ => Instruction::ASSIGN,
-                }
-            } else {
-                Instruction::ASSIGN
-            },
-            pam2: tmp[2],
-            dest: tmp[0],
-        };
-        ins.push(g);
-    }
+    'start: loop {
+        for line in input.lines() {
+            let mut tmp: Vec<String> = line
+                .split_whitespace()
+                .map(|s| s.to_string())
+                .collect::<Vec<String>>();
+            tmp.reverse();
+            let g = Gate {
+                pam1: if tmp.len() == 5 {
+                    tmp[4].clone()
+                } else {
+                    "".to_string()
+                },
+                ins: if tmp.len() == 4 {
+                    Instruction::NOT
+                } else if tmp.len() == 5 {
+                    // chooses the correct instruction based on the string
+                    match tmp[3].as_ref() {
+                        "AND" => Instruction::AND,
+                        "OR" => Instruction::OR,
+                        "NOT" => Instruction::NOT,
+                        "RSHIFT" => Instruction::RSHIFT,
+                        "LSHIFT" => Instruction::LSHIFT,
+                        _ => Instruction::ASSIGN,
+                    }
+                } else {
+                    Instruction::ASSIGN
+                },
+                pam2: tmp[2].clone(),
+                dest: tmp[0].clone(),
+            };
+            ins.push(g);
+        }
 
-    loop {
-        let mut restr = " ".to_string();
-        let mut search: &str = " ";
-        let mut res: u16 = 0;
-        let mut index = 0;
-        for (i, g) in &mut ins.iter().enumerate() {
-            if g.pam1.parse::<u16>().is_ok() && g.pam2.parse::<u16>().is_ok() {
-                match g.ins {
-                    Instruction::AND => {
-                        res = g.pam1.parse::<u16>().unwrap() & g.pam2.parse::<u16>().unwrap()
-                    }
-                    Instruction::OR => {
-                        res = g.pam1.parse::<u16>().unwrap() | g.pam2.parse::<u16>().unwrap()
-                    }
-                    Instruction::RSHIFT => {
-                        res = g.pam1.parse::<u16>().unwrap() >> g.pam2.parse::<u16>().unwrap()
-                    }
-                    Instruction::LSHIFT => {
-                        res = g.pam1.parse::<u16>().unwrap() & g.pam2.parse::<u16>().unwrap()
-                    }
-                    _ => (),
+        if part2 {
+            for g in ins.iter_mut() {
+                if g.ins == Instruction::ASSIGN && g.dest == "b" {
+                    g.pam2 = a;
+                    break;
                 }
-            } else if g.pam2.parse::<u16>().is_ok() {
-                match g.ins {
-                    Instruction::NOT => res = !g.pam2.parse::<u16>().unwrap(),
-                    Instruction::ASSIGN => res = g.pam2.parse::<u16>().unwrap(),
-                    _ => (),
-                }
-            } else {
-                continue;
             }
-            index = i;
-            break;
         }
-        if index == ins.len() {
-            break;
-        }
-        restr = res.to_string();
-        for g in &mut ins {
-            if g.pam1 == search {
-                g.pam1 = &restr[..];
+
+        loop {
+            let mut search = "".to_string();
+            let mut replace = "".to_string();
+            for (i, g) in ins.iter_mut().enumerate() {
+                search = g.dest.clone();
+                if g.pam1.parse::<u16>().is_ok() && g.pam2.parse::<u16>().is_ok() {
+                    match g.ins {
+                        Instruction::AND => {
+                            g.dest = (g.pam1.parse::<u16>().unwrap()
+                                & g.pam2.parse::<u16>().unwrap())
+                            .to_string()
+                        }
+                        Instruction::OR => {
+                            g.dest = (g.pam1.parse::<u16>().unwrap()
+                                | g.pam2.parse::<u16>().unwrap())
+                            .to_string()
+                        }
+                        Instruction::RSHIFT => {
+                            g.dest = (g.pam1.parse::<u16>().unwrap()
+                                >> g.pam2.parse::<u16>().unwrap())
+                            .to_string()
+                        }
+                        Instruction::LSHIFT => {
+                            g.dest = (g.pam1.parse::<u16>().unwrap()
+                                << g.pam2.parse::<u16>().unwrap())
+                            .to_string()
+                        }
+                        _ => continue,
+                    }
+                } else if g.pam2.parse::<u16>().is_ok() {
+                    match g.ins {
+                        Instruction::NOT => g.dest = (!g.pam2.parse::<u16>().unwrap()).to_string(),
+                        Instruction::ASSIGN => g.dest = g.pam2.clone(),
+                        _ => continue,
+                    }
+                } else {
+                    continue;
+                }
+                replace = ins.swap_remove(i).dest;
+                if search == "a" {
+                    if !part2 {
+                        println!("Part1: The signal ultimately provided to a is {}", replace);
+                        a = replace;
+                        part2 = true;
+                        continue 'start;
+                    } else {
+                        println!("Part2: The signal ultimately provided to a is {}", replace);
+                        break 'start;
+                    }
+                }
+                break;
+            }
+
+            for g in &mut ins {
+                if g.pam1 == search {
+                    g.pam1 = replace.clone();
+                }
+                if g.pam2 == search {
+                    g.pam2 = replace.clone();
+                }
             }
         }
     }
